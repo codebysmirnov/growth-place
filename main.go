33ab325366e23f08c65ed2e7496ef3536c92fb39
Smirnov <codebysmirnov@gmail.com>
2022-03-06 22:14:35 +0300

    Add database connection when server is start

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	"growth-place/config"
 )
@@ -12,6 +16,21 @@ func main() {
 		log.Fatal(err)
 	}
 	err = cfg.Validate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = gorm.Open(
+		postgres.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
+			cfg.Database.Host,
+			cfg.Database.User,
+			cfg.Database.Password,
+			cfg.Database.Name,
+			cfg.Database.Port,
+			cfg.Database.SSLMode,
+		)),
+		&gorm.Config{},
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
